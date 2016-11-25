21099b90d2cfb0e341da8f7da2f85640f0629b96
Marlon Mendes <marlonacademy@hotmail.com>
2016-11-25 18:29:18 -0200

    Class comment

--- src/com/peacecorps/malaria/app_date/IllegalMonthException.java
+++ src/com/peacecorps/malaria/app_date/IllegalMonthException.java
@@ -1,5 +1,9 @@
 package com.peacecorps.malaria.app_date;
 
+/**
+ * Class IllegalMonthException: raised exception on invalid months numbers.
+ */
+
 public class IllegalMonthException extends Exception {
     public IllegalMonthException(String message) {
         super(message);
