f0bab91ce56877c652b02dd450ba92a43e051786
Marlon Mendes <marlonacademy@hotmail.com>
2016-11-25 18:31:50 -0200

    Class comment

--- src/com/peacecorps/malaria/app_date/IllegalYearException.java
+++ src/com/peacecorps/malaria/app_date/IllegalYearException.java
@@ -1,5 +1,9 @@
 package com.peacecorps.malaria.app_date;
 
+/**
+ * Class IllegalYearException: raised exception on invalid year numbers.
+ */
+
 public class IllegalYearException extends Exception {
     public IllegalYearException(String message) {
         super(message);
